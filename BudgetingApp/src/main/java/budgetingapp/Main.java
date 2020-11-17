a75214e53981e18a7d293870abbebf7c280d1969
Manu Matila <manu.matila1997@gmail.com>
2020-11-17 17:10:54 +0200

    App initialized

---
+++ BudgetingApp/src/main/java/budgetingapp/Main.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package budgetingapp;
+
+/**
+ *
+ * @author mmatila
+ */
+public class Main {
+    public static void main(String[] args) {
+        App app = new App();
+        app.main(args);
+    }
+}
